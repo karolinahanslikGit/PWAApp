d915ef4bc039f5216b1d94bdd5748deca1513f44
karolinahanslikGit <hanslik.karolina.git@gmail.com>
2025-05-10 12:01:17 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,51 +1,37 @@
 self.addEventListener('install', function(event) {
-    console.log('Service Worker: Instalowanie...');
-   if (event.request.url.includes('favicon.ico')) {
-        return;  // Zignoruj to żądanie, nie wykonuj fetch ani cache
-    }
 
     event.waitUntil(
+
         caches.open('my-cache').then(function(cache) {
-            console.log('Service Worker: Otwarto cache');
 
-            const filesToCache = [
-                './index.html',
-                './app.js',
-                './style.css',
-                './icon.png',
-                './manifest.json'
-            ];
+            return cache.addAll([
 
-            return Promise.all(filesToCache.map(function(file) {
-                return fetch(file).then(function(response) {
-                    if (response.ok) {
-                        return cache.put(file, response);
-                    } else {
-                        console.error(`Błąd podczas pobierania pliku: ${file}`);
-                    }
-                }).catch(function(error) {
-                    console.error(`Błąd fetch dla pliku: ${file}`, error);
-                });
-            }));
+                
+                '/index.html',
+                '/app.js',
+                '/style.css',
+                '/icon.png',
+                '/sw.js'
+                
+
+            ]);
+
         })
+
     );
+
 });
 
 self.addEventListener('fetch', function(event) {
-    // Obsługa wejścia na stronę (czyli żądania dokumentu HTML)
-    if (event.request.mode === 'navigate') {
-        event.respondWith(
-            caches.match('./index.html').then(function(response) {
-                return response || fetch('./index.html');
-            })
-        );
-        return;
-    }
 
-    // Obsługa pozostałych zasobów (CSS, JS, obrazki itd.)
     event.respondWith(
+
         caches.match(event.request).then(function(response) {
+
             return response || fetch(event.request);
+
         })
+
     );
+
 });
