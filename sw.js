238a46b9bd081533a65a0ee889ca2bb22bee4638
karolinahanslikGit <hanslik.karolina.git@gmail.com>
2025-05-10 11:00:00 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,33 @@
 self.addEventListener('install', function(event) {
+    console.log('Service Worker: Instalowanie...');
+
     event.waitUntil(
         caches.open('my-cache').then(function(cache) {
-            return cache.add('/').then(() => {
-                console.log('Dodano: /');
-                return cache.add('./index.html');
-            }).then(() => {
-                console.log('Dodano: ./index.html');
-                return cache.add('./app.js');
-            }).then(() => {
-                console.log('Dodano: ./app.js');
-                return cache.add('./style.css');
-            }).then(() => {
-                console.log('Dodano: ./style.css');
-                return cache.add('./icon.png');
-            }).then(() => {
-                console.log('Dodano: ./icon.png');
-                return cache.add('./manifest.json');
-            }).then(() => {
-                console.log('Dodano: ./manifest.json');
-            }).catch(function(error) {
-                console.error('Błąd podczas dodawania plików do cache:', error);
+            console.log('Service Worker: Otwarto cache');
+            return cache.addAll([
+                '/',
+                './index.html',
+                './app.js',
+                './style.css',
+                './icon.png',
+                './manifest.json'
+            ])
+            .then(function() {
+                console.log('Service Worker: Pliki zostały dodane do cache');
+            })
+            .catch(function(error) {
+                console.error('Błąd podczas dodawania plików do cache', error);
             });
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    console.log('Service Worker: Odbieranie żądania', event.request.url);
+
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
         })
     );
 });
