3334f95c44fddb3af5765ba6bd77cdd3ebba2916
karolinahanslikGit <hanslik.karolina.git@gmail.com>
2025-05-10 11:03:37 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,30 +4,26 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open('my-cache').then(function(cache) {
             console.log('Service Worker: Otwarto cache');
-            return cache.addAll([
+            const filesToCache = [
                 '/',
                 './index.html',
                 './app.js',
                 './style.css',
                 './icon.png',
                 './manifest.json'
-            ])
-            .then(function() {
-                console.log('Service Worker: Pliki zostały dodane do cache');
-            })
-            .catch(function(error) {
-                console.error('Błąd podczas dodawania plików do cache', error);
+            ];
+
+            filesToCache.forEach(function(file) {
+                console.log('Próba dodania pliku do cache: ', file);
             });
-        })
-    );
-});
 
-self.addEventListener('fetch', function(event) {
-    console.log('Service Worker: Odbieranie żądania', event.request.url);
-
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
+            return cache.addAll(filesToCache)
+                .then(function() {
+                    console.log('Service Worker: Pliki zostały dodane do cache');
+                })
+                .catch(function(error) {
+                    console.error('Błąd podczas dodawania plików do cache', error);
+                });
         })
     );
 });
