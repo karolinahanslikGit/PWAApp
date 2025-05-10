d6a61fa5d37c7dfb38abed881512573534cda846
karolinahanslikGit <hanslik.karolina.git@gmail.com>
2025-05-10 10:42:28 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,45 +1,26 @@
-// sw.js
-
-
-
 self.addEventListener('install', function(event) {
-
     event.waitUntil(
-
         caches.open('my-cache').then(function(cache) {
-
-            return cache.addAll([
-
-                '/',
-                './index.html',
-                './app.js',
-                './style.css',
-                './icon.png',
-                './manifest.json'
-                
-           
-                
-
-            ]).catch(function(error) {
+            return cache.add('/').then(() => {
+                console.log('Dodano: /');
+                return cache.add('./index.html');
+            }).then(() => {
+                console.log('Dodano: ./index.html');
+                return cache.add('./app.js');
+            }).then(() => {
+                console.log('Dodano: ./app.js');
+                return cache.add('./style.css');
+            }).then(() => {
+                console.log('Dodano: ./style.css');
+                return cache.add('./icon.png');
+            }).then(() => {
+                console.log('Dodano: ./icon.png');
+                return cache.add('./manifest.json');
+            }).then(() => {
+                console.log('Dodano: ./manifest.json');
+            }).catch(function(error) {
                 console.error('Błąd podczas dodawania plików do cache:', error);
             });
-
         })
-
     );
-
-});
-
-self.addEventListener('fetch', function(event) {
-
-    event.respondWith(
-
-        caches.match(event.request).then(function(response) {
-
-            return response || fetch(event.request);
-
-        })
-
-    );
-
 });
